0eeb38fb3bb18f86c3378f75ab685170d07db9ae
Austin Christensen <austin.christensen37@gmail.com>
2020-07-30 14:48:37 -0500

    eslint cleanup

--- src/App.js
+++ src/App.js
@@ -1,53 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <header>
-            <ul>
-                <li>
-                    <a href='#'>Link 1</a>
-                </li>
-                <li>
-                    <a href='#'>Link 2</a>
-                </li>
-                <li>
-                    <a href='#'>Link 3</a>
-                </li>
-                <li>
-                    <a href='#'>Link 4</a>
-                </li>
-                <li>
-                    <a href='#'>Link 5</a>
-                </li>
-            </ul>
-        </header>
-        <main>
-            <section className='card-container'>
-                <article className='card-item'>
-                    <img src='https://via.placeholder.com/250' />
-                </article>
-                <article className='card-item'>
-                    <img src='https://via.placeholder.com/250' />
-                </article>
-                <article className='card-item'>
-                    <img src='https://via.placeholder.com/250' />
-                </article>
-                <article className='card-item'>
-                    <img src='https://via.placeholder.com/250' />
-                </article>
-                <article className='card-item'>
-                    <img src='https://via.placeholder.com/250' />
-                </article>
-                <article className='card-item'>
-                    <img src='https://via.placeholder.com/250' />
-                </article>
-            </section>
-        </main>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <header>
+                <ul>
+                    <li>
+                        <a href='/'>Link 1</a>
+                    </li>
+                    <li>
+                        <a href='/'>Link 2</a>
+                    </li>
+                    <li>
+                        <a href='/'>Link 3</a>
+                    </li>
+                    <li>
+                        <a href='/'>Link 4</a>
+                    </li>
+                    <li>
+                        <a href='/'>Link 5</a>
+                    </li>
+                </ul>
+            </header>
+            <main>
+                <section className='card-container'>
+                    <article className='card-item'>
+                        <img
+                            src='https://via.placeholder.com/250'
+                            alt='placeholder-item'
+                        />
+                    </article>
+                    <article className='card-item'>
+                        <img
+                            src='https://via.placeholder.com/250'
+                            alt='placeholder-item'
+                        />
+                    </article>
+                    <article className='card-item'>
+                        <img
+                            src='https://via.placeholder.com/250'
+                            alt='placeholder-item'
+                        />
+                    </article>
+                    <article className='card-item'>
+                        <img
+                            src='https://via.placeholder.com/250'
+                            alt='placeholder-item'
+                        />
+                    </article>
+                    <article className='card-item'>
+                        <img
+                            src='https://via.placeholder.com/250'
+                            alt='placeholder-item'
+                        />
+                    </article>
+                    <article className='card-item'>
+                        <img
+                            src='https://via.placeholder.com/250'
+                            alt='placeholder-item'
+                        />
+                    </article>
+                </section>
+            </main>
+        </div>
+    );
 }
 
+// eslint-disable-next-line import/no-default-export
 export default App;
