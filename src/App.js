2eb48723d7fb3ac45b37abacdbe36fce88bc16e7
Austin Christensen <austin.christensen37@gmail.com>
2020-07-30 14:31:09 -0500

    add basic, responsive styles

--- src/App.js
+++ src/App.js
@@ -5,20 +5,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <ul>
+                <li>
+                    <a href='#'>Link 1</a>
+                </li>
+                <li>
+                    <a href='#'>Link 2</a>
+                </li>
+                <li>
+                    <a href='#'>Link 3</a>
+                </li>
+                <li>
+                    <a href='#'>Link 4</a>
+                </li>
+                <li>
+                    <a href='#'>Link 5</a>
+                </li>
+            </ul>
+        </header>
+        <main>
+            <section className='card-container'>
+                <article className='card-item'>
+                    <img src='https://via.placeholder.com/250' />
+                </article>
+                <article className='card-item'>
+                    <img src='https://via.placeholder.com/250' />
+                </article>
+                <article className='card-item'>
+                    <img src='https://via.placeholder.com/250' />
+                </article>
+                <article className='card-item'>
+                    <img src='https://via.placeholder.com/250' />
+                </article>
+                <article className='card-item'>
+                    <img src='https://via.placeholder.com/250' />
+                </article>
+                <article className='card-item'>
+                    <img src='https://via.placeholder.com/250' />
+                </article>
+            </section>
+        </main>
     </div>
   );
 }
